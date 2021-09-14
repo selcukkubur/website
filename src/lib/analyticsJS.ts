32e4b2863dbe07bf2c4411197d9dbe3e09d6d79a
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2021-09-14 21:50:20 +0400

    Defer loading of Segment analytics (#159)
    * Defer loading segment analytics
    
    * Experiment: Delay loading analytics
    
    * Experiment: Load intercom separately with delay

--- src/lib/analyticsJS.ts
+++ src/lib/analyticsJS.ts
@@ -2,10 +2,25 @@ import { SegmentAnalytics } from "@segment/analytics.js-core";
 
 declare global {
   interface Window {
-    analytics?: SegmentAnalytics.AnalyticsJS;
+    analytics?: SegmentAnalytics.AnalyticsJS & {
+      load: (key: string, options?: object) => void;
+      _writeKey: string;
+    };
   }
 }
 
-const analyticsJS = () => global.window.analytics;
+let loaded = false;
+
+const analyticsJS = (): SegmentAnalytics.AnalyticsJS | void => {
+  if (!loaded) {
+    global.window.analytics?.load(global.window.analytics._writeKey, {
+      integrations: { Intercom: false },
+    });
+
+    loaded = true;
+  }
+
+  return global.window.analytics;
+};
 
 export default analyticsJS;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,10 +9,11 @@ class MyDocument extends Document {
     return { ...initialProps };
   }
 
-  renderSnippet() {
+  renderAnalyticsSnippet() {
     const opts = {
       apiKey: process.env.SEGMENT_KEY,
       page: false,
+      load: false,
     };
 
     if (process.env.NODE_ENV === "development") {
@@ -22,11 +23,27 @@ class MyDocument extends Document {
     return snippet.min(opts);
   }
 
+  renderIntercomSnippet() {
+    return `
+      var APP_ID = "${process.env.INTERCOM_KEY}";
+      var INTERCOM_DELAY = 5000;
+      window.intercomSettings = {
+        app_id: APP_ID,
+      };
+      (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){setTimeout(function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}, INTERCOM_DELAY)};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    `;
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
-          <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
+          <script
+            dangerouslySetInnerHTML={{ __html: this.renderAnalyticsSnippet() }}
+          />
+          <script
+            dangerouslySetInnerHTML={{ __html: this.renderIntercomSnippet() }}
+          />
           <link
             rel="preload"
             href="/fonts/Gellix/Gellix-ExtraBold.ttf"
