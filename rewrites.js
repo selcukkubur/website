c48a54346ec472e94d40feb6106bbf7cf4be8714
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2021-12-04 01:29:20 +0400

    Fix menu docs link (#196)

--- rewrites.js
+++ rewrites.js
@@ -1,5 +1,9 @@
 module.exports = [
   {
+    source: "/docs/",
+    destination: `${process.env.DOCS_URL || ""}`,
+  },
+  {
     source: "/docs/:slug*",
     destination: `${process.env.DOCS_URL || ""}/:slug*`,
   },
